05f07b97f974b482b7e1865566678d5ab0fe2b0a
dev_a <dev_a@gitlab.lan.fr>
2025-01-21 17:06:40 +0100

    end 2ème jour

--- auto-stash.sh
+++ auto-stash.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-## place in the PATH !!!
 
-if [[ "$1" == "-" ]];then
-  git switch -
+if [[ "$1" == "-" ]]; then
+  git switch - 
+  git 
   git stash apply -q stash^{/tmp-stash}
+  index=$(git stash list | grep -Po "\K[0-9]+\}.+tmp-stash$" | awk -F '}' '{ print $1  }')
+  git stash drop "stash{$index}"
 else
-  git stash push -u -q -m "tmp-stash";
+  git stash push -u -q -m "tmp-stash-$1";
   git switch $1
 fi

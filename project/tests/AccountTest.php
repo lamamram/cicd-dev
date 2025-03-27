d9d4d1753fa8e491f6ffe9e226121046ce45ddc6
mlamamra <mlamamra@dawan.fr>
2025-03-27 10:40:57 +0100

    TDD n°3: utilisation des fixtures old-school

--- project/tests/AccountTest.php
+++ project/tests/AccountTest.php
@@ -5,13 +5,24 @@ use PHPUnit\Framework\TestCase;
 require_once dirname(__DIR__, 1) . "/classes/Account.php";
 
 class AccountTest extends TestCase {
+    private Account $acc;
+
+    // fixture exécutée avant chaque méthode de test
+    public function setUp(): void {
+        // Arrange: Given de l'issue 
+        $this->acc = new Account(345354353, 500.);
+    }
+    
     public function testAccountWithdrawal(): void
     {
-        // Arrange: Given de l'issue 
-        $acc = new Account(345354353, 500.);
         // Act: When de l'issue
-        $acc->withdrawal(100.);
+        $this->acc->withdrawal(100.);
         // Assert: Then de l'issue
-        $this->assertEquals(400., $acc->getBalance());
+        $this->assertEquals(400., $this->acc->getBalance());
     }
+
+    // public function tearDown(): void {
+    //     // libération des variables après test
+    //     // fermer les cnx (sockets) & fichiers (memory leaks!!)
+    // }
 }

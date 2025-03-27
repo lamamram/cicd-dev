2a3175b1cb30c9b287db4a6dc605fc4261b10709
mlamamra <mlamamra@dawan.fr>
2025-03-27 10:59:39 +0100

    TDD n°3: utilisation des tests en dépendances: @depends

--- project/tests/AccountTest.php
+++ project/tests/AccountTest.php
@@ -8,17 +8,32 @@ class AccountTest extends TestCase {
     private Account $acc;
 
     // fixture exécutée avant chaque méthode de test
-    public function setUp(): void {
-        // Arrange: Given de l'issue 
-        $this->acc = new Account(345354353, 500.);
+    // public function setUp(): void {
+    //     // Arrange: Given de l'issue 
+    //     $this->acc = new Account(345354353, 500.);
+    // }
+
+    public function testSingleValidAccount(): Account {
+        $this->assertTrue(true);
+        return new Account(345354353, 500.);
     }
     
-    public function testAccountWithdrawal(): void
+    // méthode de test avec setUp
+    // public function testAccountWithdrawal(): void
+    // {
+    //     // Act: When de l'issue
+    //     $this->acc->withdrawal(100.);
+    //     // Assert: Then de l'issue
+    //     $this->assertEquals(400., $this->acc->getBalance());
+    // }
+
+    /**
+     * @depends testSingleValidAccount
+     */
+    public function testAccountWithdrawal(Account $acc): void
     {
-        // Act: When de l'issue
-        $this->acc->withdrawal(100.);
-        // Assert: Then de l'issue
-        $this->assertEquals(400., $this->acc->getBalance());
+        $acc->withdrawal(100.);
+        $this->assertEquals(400., $acc->getBalance());
     }
 
     // public function tearDown(): void {
